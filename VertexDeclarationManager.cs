fba72cb4ac63fe5de151a6e37d8e0197117f8f78
tekord <tekord@yandex.ru>
2013-09-16 20:25:10 +0400

    Added class VertexDeclarationManager

---
+++ VertexDeclarationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tekord.VertexDeclarationSystem {
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class VertexDeclarationManager {
+		static Dictionary<Type, VertexDeclaration> _store = new Dictionary<Type, VertexDeclaration>();
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public static VertexDeclaration Get<T>()
+			where T : struct, IVertexFormat
+		{
+			Type type = typeof(T);
+			VertexDeclaration result;
+			
+			_store.TryGetValue(type, out result);
+			
+			return result;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="item"></param>
+		public static void Set<T>(VertexDeclaration item)
+			where T : struct, IVertexFormat
+		{
+			_store[typeof(T)] = item;
+		}
+	}
+}
