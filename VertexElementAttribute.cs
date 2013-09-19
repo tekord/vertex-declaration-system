3b02f9a968e6f5231836ccefb7565f9cb49db654
tekord <tekord@yandex.ru>
2013-09-19 14:02:47 +0400

    Added VertexElementAttribute.cs; VertexElementFormat enumeration moved to VertexElementFormat.cs

---
+++ VertexElementAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Tekord.VertexDeclarationSystem {
+	/// <summary>
+	/// 
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public sealed class VertexElementAttribute : Attribute {
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Stream {
+			get; set;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Usage {
+			get; set;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public VertexElementFormat Format {
+			get; set;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Offset {
+			get; set;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public int UsageIndex {
+			get; set;
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="usage"></param>
+		/// <param name="format"></param>
+		/// <param name="usageIndex"></param>
+		public VertexElementAttribute(string usage, VertexElementFormat format, ushort usageIndex = 0) {
+			Usage = usage;
+			Format = format;
+			UsageIndex = usageIndex;
+		}
+	}
+}
---
+++ VertexElementFormat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Tekord.VertexDeclarationSystem {
+	/// <summary>
+	/// Defines the vertex element formats.
+	/// </summary>
+	public enum VertexElementFormat {
+		/// <summary>
+		/// Single-component, 32-bit floating-point, expanded to (float, 0, 0, 1).
+		/// </summary>
+		Single,
+		
+		/// <summary>
+		/// Two-component, 32-bit floating-point, expanded to (float, float, 0, 1).
+		/// </summary>
+		Vector2,
+		
+		/// <summary>
+		/// Three-component, 32-bit floating point, expanded to (float, float, float, 1).
+		/// </summary>
+		Vector3,
+		
+		/// <summary>
+		/// Four-component, 32-bit floating point, expanded to (float, float, float, float).
+		/// </summary>
+		Vector4,
+		
+		/// <summary>
+		/// Four-component, packed, unsigned byte, mapped from 0 to 1 range. Input is in Int32 format (ARGB) expanded to (R, G, B, A).
+		/// </summary>
+		Color,
+		
+		/// <summary>
+		/// Two-component, signed short expanded to (value, value, 0, 1).
+		/// </summary>
+		Short2,
+		
+		/// <summary>
+		/// Four-component, signed short expanded to (value, value, value, value).
+		/// </summary>
+		Short4,
+		
+		/// <summary>
+		/// Four-component, unsigned byte.
+		/// </summary>
+		Byte4
+	}
+}
